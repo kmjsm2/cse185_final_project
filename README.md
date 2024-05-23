02daa520144258b9bb218df7b5c454066aad9763
nak003 <nak003@ucsd.edu>
2024-05-22 19:43:54 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cse185_final_project
+Input gene.results file and create volcano and scatter plot of TPM. 
+## Installiation 
+
+## Usage
---
+++ volcano.py
@@ -0,0 +1,6 @@
+import argparse
+def main():
+    parser = argparse.ArgumentParser(
+		prog="volcano",
+		description="Command-line script to create scatter and volcano plot from input of gene.results file"
+	)
